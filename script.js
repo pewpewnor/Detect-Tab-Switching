bb04670bb6254c3991f0fc30b892b6e99f030505
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2025-12-05 19:24:06 +0800

    add checks

--- script.js
+++ script.js
@@ -1,60 +1,65 @@
+let enableCheckVisibilityAPI = true;
+let enableCheckFocus = true;
+let enableCheckBlur = true;
 let enableCheckActivity = false;
 
 if (typeof document.hidden !== "undefined") {
-	console.log("API enabled");
-	document.getElementById("info").innerHTML +=
-		"Page visibility API seems to be enabled (this could be untrue)";
+    console.log("API enabled");
+    document.getElementById("info").innerHTML +=
+        "Page visibility API seems to be enabled (this could be untrue)";
 } else {
-	console.log("API disabled");
-	document.getElementById("info").innerHTML +=
-		"Page visibility API seems to be disabled (still could be detected by other methods)";
+    console.log("API disabled");
+    document.getElementById("info").innerHTML +=
+        "Page visibility API seems to be disabled (still could be detected by other methods)";
 }
 
 function alertSwitchTabs(msg) {
-	console.log("You switched tabs" + msg);
-	document.getElementById("txt").innerHTML +=
-		"You switched tabs" + msg + "\n";
-	document.getElementById("title").innerHTML =
-		"You have been caught cheating";
-	document.getElementById("title").style.color =
-		"#ff0000";
+    console.log("You switched tabs" + msg);
+    document.getElementById("txt").innerHTML +=
+        "You switched tabs" + msg + "\n";
+    document.getElementById("title").innerHTML =
+        "You have been caught cheating";
+    document.getElementById("title").style.color = "#ff0000";
 }
 
-document.addEventListener("visibilitychange", () => {
-	alertSwitchTabs(" from page visibility API detect");
-});
+if (enableCheckVisibilityAPI) {
+    document.addEventListener("visibilitychange", () => {
+        alertSwitchTabs(" from page visibility API detect");
+    });
+}
 
-firstTime = true;
+if (enableCheckFocus) {
+    let firstTime = true;
+    window.addEventListener("focus", () => {
+        if (firstTime) {
+            firstTime = false;
+        } else {
+            alertSwitchTabs(" from focus detect");
+        }
+    });
+}
 
-window.addEventListener("focus", () => {
-	if (firstTime) {
-		firstTime = false;
-	} else {
-		alertSwitchTabs(" from focus detect");
-	}
-});
-
-window.addEventListener("blur", () => {
-	alertSwitchTabs(" from blur detect");
-});
+if (enableCheckBlur) {
+    window.addEventListener("blur", () => {
+        alertSwitchTabs(" from blur detect");
+    });
+}
 
 let lastActivityTime = Date.now();
 
 function handleActivity() {
-	lastActivityTime = Date.now();
+    lastActivityTime = Date.now();
 }
 
 function checkPageActivity() {
-	const currentTime = Date.now();
-	const inactiveTime = currentTime - lastActivityTime;
+    const currentTime = Date.now();
+    const inactiveTime = currentTime - lastActivityTime;
 
-	if (inactiveTime > 1000) {
-		alertSwitchTabs(
-			" from time subtraction detect (although it detects when you are inactive / doing nothing: not reliable)"
-		);
-	}
-
-	setTimeout(checkPageActivity, 1000);
+    if (inactiveTime > 1000) {
+        alertSwitchTabs(
+            " from time subtraction detect (although it detects when you are inactive / doing nothing: not reliable)",
+        );
+    }
 }
 
 // Attach event listeners to track activity
@@ -63,6 +68,6 @@ document.addEventListener("click", handleActivity);
 document.addEventListener("keydown", handleActivity);
 
 // Start checking for page activity
-if (enableCheckActivity) {
-	checkPageActivity();
+while (enableCheckActivity) {
+    setTimeout(checkPageActivity, 1000);
 }
